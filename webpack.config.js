b8eb8701b95bee19da7f57e6d307f304f3e949e3
Pat Beecher <beecherp@gmail.com>
2016-12-10 20:54:05 -0600

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+// In webpack.config.js
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: path.resolve(__dirname + '/app/index.html'),
+  filename: 'index.html',
+  inject: 'body'
+})
+module.exports = {
+  entry: [
+    './app/index.js'
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+    ]
+  },
+  output: {
+    filename: "index_bundle.js",
+    path: path.resolve(__dirname +'/dist')
+  },
+
+  plugins: [HTMLWebpackPluginConfig]
+}
