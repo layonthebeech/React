0b838b078e03a75d6704180df5992980e4f254af
Pat Beecher <beecherp@gmail.com>
2016-12-16 08:32:43 -0700

    blah

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path')
-var PORT = '8080';
+var PORT = process.env.PORT || '8080';
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
   response.sendFile(__dirname + '/dist/index.html')
 });
 
-app.listen(process.env.PORT || PORT, function(error) {
+app.listen(PORT, function(error) {
   if (error) {
     console.error(error);
   } else {
