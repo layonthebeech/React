42a9e1d316a043f01408da6ed16dcd2fdf5a5fe3
Pat Beecher <beecherp@gmail.com>
2016-12-15 23:23:22 -0700

    blsh

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var path = require('path')
+var PORT = '8080';
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/dist/index.html')
+});
+
+app.listen(PORT, function(error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  }
+});
